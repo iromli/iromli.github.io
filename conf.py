7dc3aa5c7ec0b3983c39714e8a76a3a2f895f0c7
Isman Firmansyah <isman.firmansyah@gmail.com>
2013-01-15 06:37:39 +0700

    Atom and RSS feeds

--- conf.py
+++ conf.py
@@ -14,44 +14,15 @@ FILTERS = [
 
 VIEWS = {
     '/': {'view': 'articles'},
-
     '/:year/:month/:slug/': {'view': 'entry'},
-
     '/tag/:name/': {
         'view': 'tag',
         'pagination': '/tag/:name/:num'
     },
-
-    # per tag Atom or RSS feed. Just uncomment to generate them.
-
-    # '/tag/:name/atom/': {
-    #     'filters': ['h2', 'nohyphenate'],
-    #     'view': 'atompertag'
-    # },
-    # '/tag/:name/rss/': {
-    #     'filters': ['h2', 'nohyphenate'],
-    #     'view': 'rsspertag'
-    # },
-
-    # '/atom/': {'filters': ['h2', 'nohyphenate'], 'view': 'atom'},
-    # '/rss/': {'filters': ['h2', 'nohyphenate'], 'view': 'rss'},
+    '/atom.xml': {'filters': ['nohyphenate'], 'view': 'atom'},
+    '/rss.xml': {'filters': ['nohyphenate'], 'view': 'rss'},
     # '/sitemap.xml': {'view': 'sitemap'},
 
-    # Here are some more examples
-
-    # # '/:slug/' is a slugified url of your static page's title
-    # '/:slug/': {'view': 'page'}
-
-    # # '/atom/full/' will give you a _complete_ feed of all your entries
-    # '/atom/full/': {'filters': 'h2', 'view': 'atom', 'num_entries': 1000},
-
-    # # a feed containing all entries tagges with 'python'
-    # '/rss/python/': {'filters': 'h2', 'view': 'rss',
-    #                  'if': lambda e: 'python' in e.tags}
-
-    # # a full typography features entry including MathML and Footnotes
-    # '/:year/:slug': {'filters': ['typography', 'Markdown+Footnotes+MathML'],
-    #                  'view': 'entry'}
 }
 
 THEME = 'theme'
