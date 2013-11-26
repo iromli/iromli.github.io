2cffa27c2352d8deabcf64ff3c42beb2afe71464
Isman Firmansyah <isman.firmansyah@gmail.com>
2013-11-26 17:40:57 +0700

    fixing deploy command

--- conf.py
+++ conf.py
@@ -44,7 +44,7 @@ commands = (
     "acrylamid compile",
     "git checkout gh-pages",
     "rsync -rvc --delete-after --exclude=CNAME --exclude=.git* output/ .",
-    "git commit add .",
+    "git add .",
     "git commit -am '%s'" % deploy_msg,
     "git push origin gh-pages",
     "git checkout master",
